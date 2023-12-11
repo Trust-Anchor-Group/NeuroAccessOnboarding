bdaaa4d14259723103f4016296f7882e9ef0de46
Peter Waher <peterwaher@hotmail.com>
2023-12-11 10:56:23 -0300

    Adding Identity Authenticator

---
+++ TAG.Identity.NeuroAccess/NeuroAccessAuthenticator.cs
@@ -0,0 +1,182 @@
+﻿using Paiwise;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Waher.Content;
+using Waher.IoTGateway;
+using Waher.Runtime.Inventory;
+using Waher.Runtime.Settings;
+
+namespace TAG.Identity.NeuroAccess
+{
+	/// <summary>
+	/// Service that authenticates Neuro-Access digital identity applications.
+	/// </summary>
+	public class NeuroAccessAuthenticator : IIdentityAuthenticatorService
+	{
+		/// <summary>
+		/// Service that authenticates Neuro-Access digital identity applications.
+		/// </summary>
+		public NeuroAccessAuthenticator()
+		{
+		}
+
+		#region IModule
+
+		/// <summary>
+		/// Prefix for settings keys
+		/// </summary>
+		public static readonly string Prefix = typeof(NeuroAccessAuthenticator).Namespace;
+
+		/// <summary>
+		/// Key for Onboarding neuron setting parameter.
+		/// </summary>
+		public static readonly string OnboardingNeuronKey = Prefix + ".OnboardingNeuron";
+
+		/// <summary>
+		/// Onboarding Neuron host.
+		/// </summary>
+		private static string onboardingNeuron = null;
+
+		/// <summary>
+		/// Is called when the module is being started.
+		/// </summary>
+		public Task Start()
+		{
+			return LoadConfiguration();
+		}
+
+		/// <summary>
+		/// Is called when the module is being stopped.
+		/// </summary>
+		public Task Stop()
+		{
+			onboardingNeuron = null;
+			return Task.CompletedTask;
+		}
+
+		#endregion
+
+		#region IIdentityAuthenticatorService
+
+		/// <summary>
+		/// Loads service configuration.
+		/// </summary>
+		public static async Task LoadConfiguration()
+		{
+			onboardingNeuron = await RuntimeSettings.GetAsync(OnboardingNeuronKey, string.Empty);
+		}
+
+		/// <summary>
+		/// Checks if the service supports the authentication of an identity application.
+		/// </summary>
+		/// <param name="Application">Identity application</param>
+		/// <returns>How well the application is handled by the service.</returns>
+		public Grade Supports(IIdentityApplication Application)
+		{
+			if (string.IsNullOrWhiteSpace(onboardingNeuron) || Application.NrPhotos != 0)
+				return Grade.NotAtAll;
+
+			bool HasEMail = false;
+			bool HasPhoneNr = false;
+			bool HasJid = false;
+
+			foreach (KeyValuePair<string, object> P in Application.Claims)
+			{
+				switch (P.Key)
+				{
+					case "EMAIL":
+						HasEMail = true;
+						break;
+
+					case "PHONE":
+						HasPhoneNr = true;
+						break;
+
+					case "JID":
+						HasJid = true;
+						break;
+
+					default:
+						return Grade.NotAtAll;
+				}
+			}
+
+			return (HasEMail || HasPhoneNr) && HasJid ? Grade.Ok : Grade.NotAtAll;
+		}
+
+		/// <summary>
+		/// Authenticates an identity application.
+		/// </summary>
+		/// <param name="Identity">Meta-information in the application.</param>
+		/// <param name="Photos">Photos in the application.</param>
+		/// <returns>Authentication result.</returns>
+		public async Task<IAuthenticationResult> IsValid(KeyValuePair<string, object>[] Identity, IEnumerable<IPhoto> Photos)
+		{
+			if (string.IsNullOrEmpty(onboardingNeuron))
+				return new AuthenticationResult(ErrorType.Service, "Service not configured.");
+
+			foreach (IPhoto _ in Photos)
+				return new AuthenticationResult(ErrorType.Service, "Applications with photos cannot be authenticated using this service.");
+
+			string EMail = null;
+			string PhoneNr = null;
+			string Jid = null;
+
+			foreach (KeyValuePair<string, object> P in Identity)
+			{
+				if (!(P.Value is string s))
+					return new AuthenticationResult(false);
+
+				switch (P.Key)
+				{
+					case "EMAIL":
+						EMail = s;
+						break;
+
+					case "PHONE":
+						PhoneNr = s;
+						break;
+
+					case "JID":
+						Jid = s;
+						break;
+
+					default:
+						return new AuthenticationResult(false);
+				}
+			}
+
+			if (string.IsNullOrEmpty(Jid) || (string.IsNullOrEmpty(EMail) && string.IsNullOrEmpty(PhoneNr)))
+				return new AuthenticationResult(false);
+
+			Dictionary<string, object> Request = new Dictionary<string, object>()
+			{
+				{ "RemoteEndPoint", RemoteEndPoint }
+			};
+
+			if (!string.IsNullOrEmpty(EMail))
+				Request["EMail"] = EMail;
+
+			if (!string.IsNullOrEmpty(PhoneNr))
+				Request["Nr"] = PhoneNr;
+
+			try
+			{
+				object Obj = await InternetContent.PostAsync(new Uri("https://" + onboardingNeuron + "/ID/ValidateOnboarding.ws"), Request,
+					Gateway.Certificate, 10000, new KeyValuePair<string, string>("Accept", "application/json"));
+
+				if (!(Obj is bool Result))
+					return new AuthenticationResult(ErrorType.Server, "Unexpected response received from onboarding server.");
+
+				return new AuthenticationResult(Result);
+			}
+			catch (Exception ex)
+			{
+				return new AuthenticationResult(ErrorType.Server, ex.Message);
+			}
+		}
+
+		#endregion
+	}
+}
